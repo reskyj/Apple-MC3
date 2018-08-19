7c21e7df00247f62f58495c5263dd2e799461666
Steven Muliamin <steven.muliamin@gmail.com>
2018-08-19 21:52:45 +0700

    added googleinfoplist, added Firebase configuration on appdelegate

--- MC3Group4/MC3Group4/AppDelegate.swift
+++ MC3Group4/MC3Group4/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -22,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UINavigationBar.appearance().titleTextAttributes = [NSAttributedStringKey.foregroundColor : UIColor.white]
         
         
-        
+        FirebaseApp.configure()
         
         return true
     }
