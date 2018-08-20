eb0c4e3acbff01f0602e3b932fe53677ed903e32
Steven Muliamin <steven.muliamin@gmail.com>
2018-08-20 18:39:01 +0700

    added paperonboarding view controller

---
+++ MC3Group4/MC3Group4/OnBoardView.swift
@@ -0,0 +1,16 @@
+//
+//  OnBoardView.swift
+//  MC3Group4
+//
+//  Created by Steven Muliamin on 20/08/18.
+//  Copyright © 2018 Resky Javieri. All rights reserved.
+//
+
+import UIKit
+import paper_onboarding
+
+class OnBoardView: PaperOnboarding {
+
+    
+
+}
