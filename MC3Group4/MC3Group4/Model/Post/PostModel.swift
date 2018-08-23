b87186f849c350c6d312820d8bb65e30ab1cdda6
Steven Muliamin <steven.muliamin@gmail.com>
2018-08-23 13:59:45 +0700

    added PostModel

---
+++ MC3Group4/MC3Group4/Model/Post/PostModel.swift
@@ -0,0 +1,33 @@
+//
+//  PostModel.swift
+//  MC3Group4
+//
+//  Created by Steven Muliamin on 23/08/18.
+//  Copyright © 2018 Resky Javieri. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PostModel {
+    
+    var schoolImages: [UIImage] = []
+    var roadImages: [UIImage] = []
+    var schoolName: String
+    var aboutPost: String
+    var needsPost: String
+    var addressPost: String
+    var accessPost: String
+    var notesPost: String
+    var locationName: String
+    var locationAdminArea: String
+    var locationLocality: String
+    var locationAOI: String
+    var locationLatitude: Double
+    var locationLongitude: Double
+    
+    
+    init() {
+        
+    }
+}
