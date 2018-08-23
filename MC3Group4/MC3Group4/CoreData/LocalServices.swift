37bc678ddfd79cb5a4c67b3bf42f57928bd53f51
Steven Muliamin <steven.muliamin@gmail.com>
2018-08-23 11:52:06 +0700

    added coredata entity created Model for post

---
+++ MC3Group4/MC3Group4/CoreData/LocalServices.swift
@@ -0,0 +1,49 @@
+//
+//  LocalServices.swift
+//
+//
+//  Created by Steven Muliamin on 12/05/18.
+//  Copyright © 2018 Steven Muliamin. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class LocalServices{
+    private init(){}
+    
+    static var context: NSManagedObjectContext{
+        return persistentContainer.viewContext
+    }
+    
+    static var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "LocalModel")
+        container.loadPersistentStores(completionHandler: {(storeDescription, error) in
+            if let error = error as NSError? {
+                
+                fatalError("error has occured")
+                
+            }
+            
+        })
+        
+        return container
+        
+    }()
+    
+    static func saveContext () {
+        
+        let context = persistentContainer.viewContext
+        if context.hasChanges{
+            do{
+                try context.save()
+                print("save success")
+            }
+            catch{
+                let nserror = error as NSError
+                fatalError("Error has occured")
+            }
+        }
+        
+    }
+}
---
+++ MC3Group4/MC3Group4/Model/Post/DraftEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  DraftEntity+CoreDataClass.swift
+//  
+//
+//  Created by Steven Muliamin on 23/08/18.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(DraftEntity)
+public class DraftEntity: NSManagedObject {
+
+}
