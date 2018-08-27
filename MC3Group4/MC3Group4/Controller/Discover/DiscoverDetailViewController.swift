61ef13a713c5f88ddb57714b52479c30ceffb7b1
Resky Javieri <reskyjavier@gmail.com>
2018-08-27 18:35:21 +0700

    UI

--- MC3Group4/MC3Group4/Controller/Discover/DiscoverDetailViewController.swift
+++ MC3Group4/MC3Group4/Controller/Discover/DiscoverDetailViewController.swift
@@ -10,36 +10,33 @@ import UIKit
 
 class DiscoverDetailViewController: UIViewController, UIScrollViewDelegate {
     
-    @IBOutlet weak var pageControl: UIPageControl!
-    @IBOutlet weak var scrollView: UIScrollView!
-    
-    var images: [String] = ["Gambar-1", "Gambar-2"]
-    var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        pageControl.numberOfPages = images.count
-        for index in 0..<images.count {
-            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
-            frame.size = scrollView.frame.size
-            
-            let imageView = UIImageView(frame: frame)
-            imageView.image = UIImage(named: images[index])
-            self.scrollView.addSubview(imageView)
-        }
-        scrollView.contentSize = CGSize(width: scrollView.frame.size.width * CGFloat(images.count), height: scrollView.frame.size.height)
-        scrollView.delegate = self
-        
-
-    }
-    
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        var pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
-        pageControl.currentPage = Int(pageNumber)
-    }
-    
-    
-    
-    
+//    @IBOutlet weak var pageControl: UIPageControl!
+//    @IBOutlet weak var scrollView: UIScrollView!
+//
+//    var images: [String] = ["Gambar-1", "Gambar-2"]
+//    var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        pageControl.numberOfPages = images.count
+//        for index in 0..<images.count {
+//            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
+//            frame.size = scrollView.frame.size
+//
+//            let imageView = UIImageView(frame: frame)
+//            imageView.image = UIImage(named: images[index])
+//            self.scrollView.addSubview(imageView)
+//        }
+//        scrollView.contentSize = CGSize(width: scrollView.frame.size.width * CGFloat(images.count), height: scrollView.frame.size.height)
+//        scrollView.delegate = self
+//
+//
+//    }
+//
+//    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+//        var pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
+//        pageControl.currentPage = Int(pageNumber)
+//    }
+//
     
 }
